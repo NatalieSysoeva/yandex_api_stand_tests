5edabc5207a92b37cbbf8e28e650c69dcb89f4ec
Natalie <natalia227@mail.ru>
2023-10-05 22:16:00 +0700

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://8f1b9c2a-4a80-48d1-b538-9eecdc89e9f3.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
